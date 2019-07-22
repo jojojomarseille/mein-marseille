098ab4d44e774f72a0e78b2ad36ab088ebc8572c
Joachim Bourquardez <j.bourquardez@laposte.net>
2019-07-22 15:01:14 +0200

    Add visit model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/contact'
+  get 'pages/about'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  root to: 'pages#home'
+  get 'visits', to: 'visits#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20190722125420_create_visits.rb
@@ -0,0 +1,11 @@
+class CreateVisits < ActiveRecord::Migration[5.2]
+  def change
+    create_table :visits do |t|
+      t.string :name
+      t.string :description
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
